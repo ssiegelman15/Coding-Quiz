0146c66b5f07e37da13c45197be47a8d6d08b7b1
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-14 21:26:41 -0600

    added startTimer and endGame functions

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -12,13 +12,34 @@ var highscorePage = document.querySelector(".highscorePage");
 var goBack = document.querySelector(".goBack");
 var clearHighscores = document.querySelector(".clearHighscores");
 
+var isOver = false;
+var timer;
+var timerElement;
+
 function startGame() {
   opener.classList.add("hidden");
   quizSequence.classList.remove("hidden");
+  timerElement = 10;
+  startTimer()
 }
 
+function startTimer() {
+  timer = setInterval(function() {
+    timerElement--;
+    timeLeft.textContent = timerElement;
+    // Tests if time has run out
+    if (timerElement === 0) {
+      // Clears interval
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+}
 
-
+function endGame() {
+  quizSequence.classList.add("hidden");
+  enterInitials.classList.remove("hidden");
+}
 
 
 startButton.addEventListener("click", startGame);
