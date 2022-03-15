c1253ef75d8d26c36234a6eff90eddb4732a1b74
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-14 21:14:21 -0600

    created many more js variables

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,20 @@
 var timeLeft = document.querySelector(".timeLeft");
 var startButton = document.querySelector(".startButton");
+var highScores = document.querySelector("#highScores");
+var opener = document.querySelector(".opener");
+var quizSequence = document.querySelector(".quizSequence");
+var questionText = document.querySelector(".questionText");
+var options = document.querySelector(".options");
+var enterInitials = document.querySelector(".enterInitials");
+var finalScore = document.querySelector("#finalScore");
+var submitScore = document.querySelector(".submitScore");
+var highscorePage = document.querySelector(".highscorePage");
+var goBack = document.querySelector(".goBack");
+var clearHighscores = document.querySelector(".clearHighscores");
+
+function startGame() {
+
+}
 
 
 
